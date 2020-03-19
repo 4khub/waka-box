5833782654b5a03c022f33355e103a558989616c
tt <96486d9b@gmail.com>
2020-03-19 21:29:32 +0800

    feat: custom progress bar

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function updateGist(stats) {
     const line = [
       name.padEnd(11),
       time.padEnd(14),
-      generateBarChart(percent, 21),
+      generateBarChart(percent, 22),
       String(percent.toFixed(1)).padStart(5) + "%"
     ];
 
@@ -49,7 +49,6 @@ async function updateGist(stats) {
       gist_id: gistId,
       files: {
         [filename]: {
-          filename: `📊 Weekly development breakdown`,
           content: lines.join("\n")
         }
       }
@@ -60,18 +59,9 @@ async function updateGist(stats) {
 }
 
 function generateBarChart(percent, size) {
-  const syms = "░▏▎▍▌▋▊▉█";
-
-  const frac = Math.floor((size * 8 * percent) / 100);
-  const barsFull = Math.floor(frac / 8);
-  if (barsFull >= size) {
-    return syms.substring(8, 9).repeat(size);
-  }
-  const semi = frac % 8;
-
-  return [syms.substring(8, 9).repeat(barsFull), syms.substring(semi, semi + 1)]
-    .join("")
-    .padEnd(size, syms.substring(0, 1));
+  const syms = "░█";
+  const frac = Math.floor((size * percent) / 100);
+  return syms.substring(1, 2).repeat(frac).padEnd(size, syms.substring(0, 1));
 }
 
 (async () => {
